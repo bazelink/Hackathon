89118f5530c7978fb633407c85616fe82bf7013c
Kinyi <kinyi9461@gmail.com>
2025-12-11 15:44:43 +0300

    first

---
+++ AuthenticationSystem/frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // ensure all JS/TSX files are scanned
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
